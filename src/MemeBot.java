e09f60a21a4c80a87122bde2b6d351ed3ea94f3c
Daniel Cormier <drc5640@rit.edu>
2016-12-24 00:36:59 -0500

    made it connect and send the message, does not currently disconnect afterwards

--- src/MemeBot.java
+++ src/MemeBot.java
@@ -2,6 +2,7 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
@@ -31,12 +32,25 @@ public class MemeBot extends ListenerAdapter{
 	
 	@Override
 	public void onGuildVoiceJoin(GuildVoiceJoinEvent event){
-		VoiceChannel voiceChan = event.getChannelJoined();
-		connectTo(voiceChan);
-		Guild g = event.getGuild();
-		TextChannel chan = g.getTextChannelById("261176936510783488");
-		chan.sendMessage("!eb sodiepop");
-		disconnectFrom(voiceChan);
+        Member mem = event.getMember();
+        if(!mem.getUser().isBot()){
+            VoiceChannel voiceChan = event.getChannelJoined();
+            connectTo(voiceChan);
+            try{
+                Thread.sleep(500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            Guild g = event.getGuild();
+            TextChannel chan = g.getTextChannelById("261176936510783488");
+            chan.sendMessage("!eb sodiepop").queue();
+            try{
+                Thread.sleep(500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            disconnectFrom(voiceChan);
+        }
 	}
 
     private void connectTo(VoiceChannel channel){
