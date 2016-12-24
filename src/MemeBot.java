5fc5b6a414f2472f53bd2f9baa800c8498398f95
Daniel Cormier <drc5640@rit.edu>
2016-12-24 02:14:37 -0500

    made bot kill itself after command execution (???)

--- src/MemeBot.java
+++ src/MemeBot.java
@@ -1,43 +1,46 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.audio.AudioWebSocket;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.entities.impl.JDAImpl;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.managers.AudioManager;
 
 public class MemeBot extends ListenerAdapter{
-	
-	private static String token;
-	
-	public static void main(String[] args){
+    
+    private static String token;
+    private AudioManager manager;
+    
+    public static void main(String[] args){
         if(args.length != 1){
             System.out.println("Usage: java MemeBot token");
             System.exit(1);
         }
         token=args[0];
-		try{
-			JDA jda = new JDABuilder(AccountType.BOT)
-					.setToken(token)
-					.addListener(new MemeBot())
-					.buildBlocking();
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void onGuildVoiceJoin(GuildVoiceJoinEvent event){
+        try{
+            JDA jda = new JDABuilder(AccountType.BOT)
+                    .setToken(token)
+                    .addListener(new MemeBot())
+                    .buildBlocking();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void onGuildVoiceJoin(GuildVoiceJoinEvent event){
         Member mem = event.getMember();
         if(!mem.getUser().isBot()){
             VoiceChannel voiceChan = event.getChannelJoined();
             connectTo(voiceChan);
             try{
-                Thread.sleep(500);
+                Thread.sleep(1000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
@@ -45,22 +48,22 @@ public class MemeBot extends ListenerAdapter{
             TextChannel chan = g.getTextChannelById("261176936510783488");
             chan.sendMessage("!eb sodiepop").queue();
             try{
-                Thread.sleep(500);
+                Thread.sleep(3000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
-            disconnectFrom(voiceChan);
+            JDAImpl temp = (JDAImpl)chan.getJDA();
+            temp.getAudioManagerMap().forEach((guildID, mng) -> mng.closeAudioConnection());
+            temp.getClient().close();
         }
-	}
+    }
 
     private void connectTo(VoiceChannel channel){
-        AudioManager manager = channel.getGuild().getAudioManager();
+        if(manager == null){
+            manager = channel.getGuild().getAudioManager();
+        }
         manager.openAudioConnection(channel);
+        manager.setAutoReconnect(false);
     }
 
-    private void disconnectFrom(VoiceChannel channel){
-        AudioManager manager = channel.getGuild().getAudioManager();
-        manager.closeAudioConnection();
-        
-    }
 }
