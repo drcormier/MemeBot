e702ddef008b73e688953be95e0dc342e16dbc8a
Daniel Cormier <drc5640@rit.edu>
2016-12-24 14:25:53 -0500

    added some comments removed test code

--- src/MemeBot.java
+++ src/MemeBot.java
@@ -3,23 +3,26 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.audio.hooks.ConnectionListener;
 import net.dv8tion.jda.core.audio.hooks.ConnectionStatus;
-import net.dv8tion.jda.core.audio.hooks.ListenerProxy;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.entities.VoiceChannel;
-import net.dv8tion.jda.core.entities.impl.JDAImpl;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.managers.AudioManager;
 
 public class MemeBot extends ListenerAdapter{
     
+    // the token associated with the bot
     private static String token;
+    // the audiomanager for the guild
     private AudioManager manager;
+    // the boolean to disconnect
     public static boolean dc = false;
+    // the object to lock onto
     public static Object lock = new Object();
+    // the list of airhorn solutions commands
     private final String[] airhorns = {
             "!airhorn default",
             "!airhorn reverb",
@@ -65,10 +68,12 @@ public class MemeBot extends ListenerAdapter{
     
     public static void main(String[] args){
         if(args.length != 1){
-            System.out.println("Usage: java MemeBot token");
+            System.out.println("Usage: java -jar MemeBot.jar token");
             System.exit(1);
         }
+        // add token from CLA
         token=args[0];
+        // build instance of JDA
         try{
             JDA jda = new JDABuilder(AccountType.BOT)
                     .setToken(token)
@@ -80,49 +85,74 @@ public class MemeBot extends ListenerAdapter{
         }
     }
     
+    /**
+     * Do stuff when a user connects to a voice channel in a guild (discord server).
+     * In this instance, the bot will join the channel, send a random airhorn solutions bot command,
+     * wait to connect, then disconnect.
+     * @param event the GuildVoiceJoinEvent that is associated with this event
+     */
     @Override
     public void onGuildVoiceJoin(GuildVoiceJoinEvent event){
+        // ensure that we do not disconnect immediately after beginning a connection
         dc = false;
-        long started = System.currentTimeMillis();
         Member mem = event.getMember();
+        // ensure joinee is not a bot
         if(!mem.getUser().isBot()){
             VoiceChannel voiceChan = event.getChannelJoined();
+            // connect to the voice channel
             connectTo(voiceChan);
+            // wait a little bit
             try{
                 Thread.sleep(1000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
+            // send the message to the bot channel
             Guild g = event.getGuild();
             TextChannel chan = g.getTextChannelById("261176936510783488");
             chan.sendMessage(getRandomAirhorn()).queue();
-            
+            // wait to disconnect in a separate thrad
             new Thread(() -> waitToDisconnect()).start();
-            //JDAImpl temp = (JDAImpl)chan.getJDA();
-            //temp.getAudioManagerMap().forEach((guildID, mng) -> mng.closeAudioConnection());
-            //temp.getClient().close();
             
         }
     }
 
+    /**
+     * Connect to a given channel.
+     *
+     * @param channel the voicechannel to connect to.
+     */
     private void connectTo(VoiceChannel channel){
         if(manager == null){
             manager = channel.getGuild().getAudioManager();
         }
-        System.out.println(manager.getQueuedAudioConnection());
-        //ListenerProxy lp = new ListenerProxy();
+        // make a new connection listener instance
         MemeListener ml = new MemeListener();
+        // give the audiomanager the connection listener
         manager.setConnectionListener(ml);
+        // open audio connection
         manager.openAudioConnection(channel);
-        System.out.println(manager.getQueuedAudioConnection());
     }
 
+    /**
+     * Get a random airhorn solutions bot command
+     *
+     * @return the int index to pull the command from
+     */
     private String getRandomAirhorn(){
         int randInt = (int) (airhorns.length*Math.random());
         return airhorns[randInt];
     }
 
+    /**
+     * Wait to disconnect from the audio connection.
+     * If the bot disconnects before the connection is fully connected,
+     * the bot will be stuck in the channel. As such, we wait until
+     * the bot is connected before disconnecting.
+     * NOTE: must be run in a separate thread.
+     */
     private void waitToDisconnect(){
+        // wait until we can disconnect
         synchronized(lock){
             while(dc == false){
                 try{
@@ -131,12 +161,20 @@ public class MemeBot extends ListenerAdapter{
                     e.printStackTrace();
                 }
             }
+            // disconnect
             manager.closeAudioConnection();
 
         }
     }
 }
 
+/**
+ * An implementation of ConnectionListener designed to send
+ * the instance of MemeBot a signal to disconnect.
+ * To do this, we use OnStatusChange.
+ * @author Daniel Cormier
+ * @author Cosmo Viola
+ */
 class MemeListener implements ConnectionListener{
 
     @Override
@@ -147,8 +185,9 @@ class MemeListener implements ConnectionListener{
 
     @Override
     public void onStatusChange(ConnectionStatus arg0) {
-        System.out.println(arg0);
+        // if we are connected
         if(arg0.equals(ConnectionStatus.CONNECTED)){
+            // tell the disconnect thread to disconnect
             synchronized(MemeBot.lock){
                 MemeBot.dc = true;
                 MemeBot.lock.notifyAll();
